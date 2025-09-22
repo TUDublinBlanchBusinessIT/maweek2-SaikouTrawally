ffb5c778237eca5bc185dc7ef31469efb8d8c040
SaikouTrawally <B00161930@mytudublin.ie>
2025-09-22 16:11:28 +0100

    Add files via upload
    Step 1

---
+++ App - Copy.js
@@ -0,0 +1,14 @@
+import {Stylesheet,Text,View } from 'react-native';
+
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <View>
+      <Text> Hello,World </Text>
+    </View>
+  );
+}
